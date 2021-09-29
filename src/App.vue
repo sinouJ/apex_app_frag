68034cf08b4e87e0ad5c488f0747e00ba06049b8
= <=>
2021-09-29 19:20:09 +0200

    home view

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,19 @@
 <template>
   <Navbar />
-  <Title title="FRAG" class="container"/>
-  <table-departements :data_departements="data_departements" />
+  <home/>
 </template>
 
 <script>
-import Title from './atoms/Title'
-import TableDepartements from './components/TableDepartements.vue'
+import Home from './views/Home.vue'
 import Navbar from './components/Navbar.vue'
 
 import './sass/main.scss'
-const url = process.env.NODE_ENV == 'development' ? 'http://localhost:2222/api/departements' : 'https://api-apex-frag.herokuapp.com/api/departements'
 
 export default {
   name: 'App',
   components: {
-    Title,
-    TableDepartements,
-    Navbar
-  },
-  data: function() {
-    return {
-      data_departements: []
-    }
-  },
-  async mounted() {
-    console.log(this.data_departements)
-    const res = await fetch(url)
-    const fetched_data = await res.json()
-    this.data_departements = fetched_data
-    console.log(this.data_departements)
-
-    console.log(process.env)
+    Navbar,
+    Home
   }
 }
 </script>
