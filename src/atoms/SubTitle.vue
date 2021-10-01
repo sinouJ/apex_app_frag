d9632cde772282150a23bb0ac27a80f5a3ad53ce
= <=>
2021-10-02 00:23:24 +0200

    ui community view

---
+++ src/atoms/SubTitle.vue
@@ -0,0 +1,20 @@
+<template>
+  <h2>{{title}}</h2>
+</template>
+
+<script>
+export default {
+    name: 'SubTitle',
+    props: {
+        title: String
+    }
+}
+</script>
+
+<style>
+    h2 {
+        text-align: center;
+        margin: 0 0 15px 0;
+
+    }
+</style>
