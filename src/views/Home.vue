1bd9084bb47a7382ec30c354600e89a5308692d3
= <=>
2021-10-02 11:10:18 +0200

    init header

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,21 @@
 <template>
-  <Title title="FRAG" class="container"/>
-  <table-departements :data_departements="data_departements" />
+    <div class="container">
+        <Header title="FRAG"/>
+        <table-departements :data_departements="data_departements" />
+    </div>
 </template>
 
 <script>
-import Title from '../atoms/Title'
 import TableDepartements from '../components/TableDepartements.vue'
+import Header from '../components/Header.vue'
 
 const url = process.env.NODE_ENV == 'development' ? 'http://localhost:2222/api/departements' : 'https://api-apex-frag.herokuapp.com/api/departements'
 
 export default {
     name: "Home",
     components: {
-        Title,
-        TableDepartements
+        TableDepartements,
+        Header
     },
     data: function() {
         return {
