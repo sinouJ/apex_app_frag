ea643f778d1328dca5863ec6245c58bf427fdeb9
= <=>
2021-10-04 00:59:11 +0200

    get user by id method

---
+++ src/views/PlayerStats.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="playerStats container">
+      <Title :title="player.game_username"/>
+  </div>
+</template>
+
+<script>
+import Title from '../atoms/Title.vue'
+
+export default {
+    name: 'PlayerStats',
+    components: {
+        Title
+    },
+    data: function() {
+        return {
+            player: [],
+            player_data: {}
+        }
+    },
+    mounted() {
+        
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .playerStats {
+
+    }
+</style>
