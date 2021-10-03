0cdc8cb7f7268b6ab372a34024abc5d174257bee
= <=>
2021-10-04 01:01:48 +0200

    add route player stats

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Community from '../views/Community.vue'
+import PlayerStats from '../views/PlayerStats.vue'
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: '/community',
     name: 'Community',
     component: Community
+  },
+  {
+    path: 'stats/:id',
+    name: 'PlayerStats',
+    component: PlayerStats
   }
 ]
 
