6a9d0f11f9b26676340353d812d534ce14e2d404
= <=>
2021-10-17 13:15:58 +0200

    test

--- src/router/index.js
+++ src/router/index.js
@@ -2,22 +2,38 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Community from '../views/Community.vue'
 import Register from '../views/Register'
+import Login from '../views/Login'
+
+const isAuthenticated = false
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    beforeEnter: (to, from, next) => {
+      if (to.name !== 'Login' && !isAuthenticated) next({name: 'Login'})
+      else next()
+    }
   },
   {
     path: '/community',
     name: 'Community',
-    component: Community
+    component: Community,
+    beforeEnter: (to, from, next) => {
+      if (to.name !== 'Login' && !isAuthenticated) next ({name: 'Login'})
+      else next()
+    }
   },
   {
     path: '/register',
     name: 'Register',
     component: Register
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
   }
 ]
 
