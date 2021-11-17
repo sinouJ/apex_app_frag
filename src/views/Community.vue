044251c60c96e0ab6fc2133a98e2efcf6f068c34
SinouJ <sinoujordan@gmail.com>
2021-11-17 18:29:37 +0100

    automation community page

--- src/views/Community.vue
+++ src/views/Community.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <Header title="Community"/>
-    <card-player :game_username="username" :id="id"/>
+    <card-player v-for="user in users" :key="user.game_username" :game_username="user.game_username"/>
   </div>
 </template>
 
@@ -10,17 +10,25 @@ import Header from '../components/Header.vue'
 import CardPlayer from '../components/CardPlayer.vue'
 
 export default {
-    name: "Community",
-    components: {
-      CardPlayer,
-      Header
-    },
-    data: function() {
-      return {
-        username: 'Nouuns',
-        id: 10
-      }
+  name: "Community",
+  components: {
+    CardPlayer,
+    Header
+  },
+  data: function() {
+    return {
+      users: []
     }
+  },
+  async mounted() {
+    const url = process.env.NODE == 'develoment' ? 'http://localhost:2222/api/user/all' : 'https://api-apex-frag.herokuapp.com/api/user/all'
+
+    const req = await fetch(url)
+
+    const res = await req.json()
+
+    this.users = res
+  }
 }
 </script>
 
