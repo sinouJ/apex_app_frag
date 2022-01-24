d6b1709de3df0f61f4aa2810bd48bd1141e8112c
Jordan Sinou <sinoujordan@gmail.com>
2022-01-24 10:53:05 +0100

    missing coma

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -27,7 +27,7 @@ const FetchData = {
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
-            }
+            },
             body: JSON.stringify(body)
         })
     }
