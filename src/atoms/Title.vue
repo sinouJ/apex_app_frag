08c4fa531eabcead4eba98cbd2bf058990213076
Jordan Sinou <sinoujordan@gmail.com>
2022-02-24 20:55:54 +0100

    edit title style

--- src/atoms/Title.vue
+++ src/atoms/Title.vue
@@ -11,5 +11,9 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Rubik+Mono+One&display=swap');
+h1 {
+  font-family: 'Rubik Mono One', sans-serif;
+}
 </style>
