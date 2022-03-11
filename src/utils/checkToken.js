4fdc0b41ea6850739d34f6d8563c7dc86798a419
Jordan Sinou <sinoujordan@gmail.com>
2022-03-11 12:12:21 +0100

    fix env url

--- src/utils/checkToken.js
+++ src/utils/checkToken.js
@@ -1,18 +1,19 @@
 import Cookies from 'js-cookie'
+import url from './urlUtils'
 
 const getUsername = async () => {
     let isAuthenticated = false
     const token = Cookies.get('token')
-    const url = 'http://localhost:2222/api/user/usernameAuth'
+    const req_url = url+'user/usernameAuth'
 
-    const req = await fetch(url, {
+    const req_url = await fetch(url, {
         method: 'GET',
         headers: {
             authorization: token
         }
     })
 
-    const res = await req.json()
+    const res = await req_url.json()
 
     if (res.status === 200) {
         isAuthenticated = true
