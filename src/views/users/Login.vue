df265c4410c7d2f082f03e07db82da27689924c6
Jordan Sinou <sinoujordan@gmail.com>
2022-03-12 13:29:50 +0100

    beautify

--- src/views/users/Login.vue
+++ src/views/users/Login.vue
@@ -1,127 +1,125 @@
 <template>
-    <div class="login">
-        <vue-notification-list position="top-left"></vue-notification-list>
-        <img src="../../assets/logo.png">
-        <form @submit.prevent="validForm">
-            <div class="checkUsername">
-                <input-text 
-                    id="checkUsername"
-                    label="Username"
-                    @update="updateConnexionUsername"
-                />
-                <input-password
-                    id="pwd"
-                    label="Password"
-                    @update="updatePassword"
-                />
-            </div>
-            <div class="buttonContainer">
-                <Button @clicked="validForm" txt="Login"/>
-            </div>
-        </form>
-    </div>
+  <div class="login">
+    <vue-notification-list position="top-left"></vue-notification-list>
+    <img src="../../assets/logo.png" />
+    <form @submit.prevent="validForm">
+      <div class="checkUsername">
+        <input-text
+          id="checkUsername"
+          label="Username"
+          @update="updateConnexionUsername"
+        />
+        <input-password id="pwd" label="Password" @update="updatePassword" />
+        <router-link to="/register"> Inscription </router-link>
+      </div>
+      <div class="buttonContainer">
+        <Button @clicked="validForm" txt="Login" />
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
 // Components
-import InputPassword from '../../atoms/forms/InputPassword.vue'
-import InputText from '../../atoms/forms/InputText.vue'
-import Button from '../../atoms/buttons/Button.vue'
+import InputPassword from "../../atoms/forms/InputPassword.vue";
+import InputText from "../../atoms/forms/InputText.vue";
+import Button from "../../atoms/buttons/Button.vue";
 
 // Cookies
-import Cookies from 'js-cookie'
+import Cookies from "js-cookie";
 
 // Toasts
-import { useNotificationStore } from '@dafcoe/vue-notification'
-const { setNotification } = useNotificationStore()
-import notificationStore from '../../assets/notification.store'
+import { useNotificationStore } from "@dafcoe/vue-notification";
+const { setNotification } = useNotificationStore();
+import notificationStore from "../../assets/notification.store";
 
 export default {
-    name: 'Login',
-    components: {
-        InputPassword,
-        InputText,
-        Button
+  name: "Login",
+  components: {
+    InputPassword,
+    InputText,
+    Button,
+  },
+  data: function () {
+    return {
+      formData: {
+        connexion_username: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    updateConnexionUsername: function (e) {
+      this.formData.connexion_username = e;
     },
-    data: function() {
-        return {
-            formData: {
-                connexion_username: '',
-                password: ''
-            },
-        }
+    updatePassword: function (e) {
+      this.formData.password = e;
     },
-    methods: {
-        updateConnexionUsername: function(e) {
-            this.formData.connexion_username = e
+    validForm: async function () {
+      const url =
+        process.env.NODE_ENV == "development"
+          ? "http://localhost:2222/api/user/login"
+          : "https://api-apex-frag.herokuapp.com/api/user/login";
+      const req = await fetch(url, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
         },
-        updatePassword: function(e) {
-            this.formData.password = e
-        },
-        validForm: async function() {
-            const url = process.env.NODE_ENV == 'development' ? 'http://localhost:2222/api/user/login' : 'https://api-apex-frag.herokuapp.com/api/user/login'
-            const req = await fetch(url, {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    password: this.formData.password,
-                    connexion_username: this.formData.connexion_username
-                })
-            })
-            
-            const res = await req.json()
+        body: JSON.stringify({
+          password: this.formData.password,
+          connexion_username: this.formData.connexion_username,
+        }),
+      });
 
-            if (res.status == 200) {
-                Cookies.set('token', res.token)
-                this.$router.push('/')
-                return
-            }
-            else if (res.status == 403) {
-                setNotification(notificationStore.invalidPassword)
-                return
-            }
-            else if (res.status == 404) {
-                setNotification(notificationStore.userNotFound)
-                return
-            }
-        }
+      const res = await req.json();
+
+      if (res.status == 200) {
+        Cookies.set("token", res.token);
+        this.$router.push("/");
+        return;
+      } else if (res.status == 403) {
+        setNotification(notificationStore.invalidPassword);
+        return;
+      } else if (res.status == 404) {
+        setNotification(notificationStore.userNotFound);
+        return;
+      }
     },
-}
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-    .login {
-        height: calc(100vh - 97px);
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        position: relative;
+.login {
+  height: calc(100vh - 97px);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: relative;
 
-        img {
-            position: absolute;
-            top: 30px;
-            left: 0;
-            right: 0;
-            margin: 0 auto;
-            width: 70%;
-        }
+  img {
+    position: absolute;
+    top: 30px;
+    left: 0;
+    right: 0;
+    margin: 0 auto;
+    width: 70%;
+  }
 
-        .buttonContainer {
-            position: absolute;
-            bottom: 40px;
-            left: 0;
-            right: 0;
-            margin: 0 auto;
-            display: flex;
-            justify-content: center;
+  .buttonContainer {
+    position: absolute;
+    bottom: 40px;
+    left: 0;
+    right: 0;
+    margin: 0 auto;
+    display: flex;
+    justify-content: center;
 
-            .button {
-                color: white;
-                margin: 0 auto;
-            }
-        }
+    .button {
+      color: white;
+      margin: 0 auto;
     }
+  }
+}
 </style>
