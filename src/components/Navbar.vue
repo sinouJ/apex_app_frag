6e46c9702d4938fed6608661d88f62b30ff964b1
Jordan Sinou <sinoujordan@gmail.com>
2022-03-14 01:28:57 +0100

    active link on nav

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,17 +4,20 @@
           <ul>
             <li>
                 <router-link to="/">
-                    <img src="../assets/icons/on_home.svg">
+                    <img class="active" src="../assets/icons/on_home.svg">
+                    <img class="inactive" src="../assets/icons/off_home.svg">
                 </router-link>
             </li>
             <li>
                 <router-link to="/community">
-                    <img src="../assets/icons/white_3_user.svg">
+                    <img class="active" src="../assets/icons/on_3_user.svg">
+                    <img class="inactive" src="../assets/icons/off_3_user.svg">
                 </router-link>
             </li>
             <li>
                 <router-link to="/departements">
-                    <img src="../assets/icons/white_paper.svg">
+                    <img class="active" src="../assets/icons/on_paper.svg">
+                    <img class="inactive" src="../assets/icons/off_paper.svg">
                 </router-link>
             </li>
           </ul>
@@ -38,17 +41,33 @@ export default {
         position: fixed;
         z-index: 999;
         bottom: 0;
-        background-color: $dark;
-        border-top: 1px solid #636071;
+        background-color: $light;
+        box-shadow: $shadow_top;
 
         ul {
             padding-bottom: 10px;
 
-            img {
-                display: block;
-                width: 36px;
-                height: 36px;
+            a {
+                &.router-link-active {
+                    .active {
+                        display: block;
+                    }
+                    .inactive {
+                        display: none;
+                    }
+                }
+
+                img {
+                    width: 36px;
+                    height: 36px;
+
+                    &.active {
+                        display: none;
+                    }
+                }
             }
+
+            
         }
 
         
