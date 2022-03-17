1b1d0ceaa3841c989262850279b25b7dae449987
Jordan Sinou <sinoujordan@gmail.com>
2022-03-17 20:34:55 +0100

    loader on community page

--- src/views/users/Community.vue
+++ src/views/users/Community.vue
@@ -1,31 +1,40 @@
 <template>
   <div>
     <Header title="Community"/>
-    <div class="container">
+    <loader v-if="loading"/>
+    <div v-else class="container">
       <card-player v-for="user in users" :key="user.game_username" :game_username="user.game_username"/>
     </div>
   </div>
 </template>
 
 <script>
+
+// Components
 import Header from '../../components/Header.vue'
 import CardPlayer from '../../components/Cards/CardPlayer.vue'
+
+// Utils
 import {FetchData} from '@/utils/fetch'
+import Loader from '../../components/Loader.vue'
 
 export default {
   name: "Community",
   components: {
     CardPlayer,
-    Header
+    Header,
+    Loader
   },
   data: function() {
     return {
-      users: []
+      users: [],
+      loading: true
     }
   },
   async mounted() {
     const res = await FetchData.get("user/all")
     this.users = res.users
+    this.loading = false
   }
 }
 </script>
