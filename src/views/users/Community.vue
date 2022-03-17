ec1e803e879150140ec9113cc460611610fa643a
Jordan Sinou <sinoujordan@gmail.com>
2022-03-17 19:26:57 +0100

    design community view

--- src/views/users/Community.vue
+++ src/views/users/Community.vue
@@ -2,20 +2,32 @@
   <div>
     <Header title="Community"/>
     <div class="container">
+      <card-community title="Annonce">
+        <template v-slot:main>
+          <p>
+            Coming soon
+          </p>
+        </template>
+      </card-community>
       <card-player v-for="user in users" :key="user.game_username" :game_username="user.game_username" :color="cardColorArray[Math.floor(Math.random() * cardColorArray.length)]"/>
     </div>
   </div>
 </template>
 
 <script>
+// Components
 import Header from '../../components/Header.vue'
 import CardPlayer from '../../components/Cards/CardPlayer.vue'
+import CardCommunity from '../../components/Cards/CardCommunity.vue'
+
+// Utils
 import {FetchData} from '@/utils/fetch'
 
 export default {
   name: "Community",
   components: {
     CardPlayer,
+    CardCommunity,
     Header
   },
   data: function() {
@@ -31,6 +43,11 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import '../../sass/helpers/variables';
 
+  .cardCommunity {
+    margin-bottom: 20px;
+    color: $light;
+  }
 </style>
