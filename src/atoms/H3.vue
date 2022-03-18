6fa63ecdcaf5107e6977184f470379c8e52bfebd
Jordan Sinou <sinoujordan@gmail.com>
2022-03-18 20:38:01 +0100

    create h3 style

---
+++ src/atoms/H3.vue
@@ -0,0 +1,34 @@
+<template>
+  <h3 :class="light ? 'light' : 'dark'">{{title}}</h3>
+</template>
+
+<script>
+export default {
+    name: 'H3',
+    props: {
+        title: String,
+        light: Boolean
+    }
+}
+</script>
+
+<style lang="scss">
+    @import '../sass/helpers/variables';
+
+    h3 {
+        font-family: 'Rubik Mono One', sans-serif;
+        text-transform: uppercase;
+        font-size: 18px;
+        margin: 0;
+        position: relative;
+        z-index: 2;
+
+        &.dark {
+            color: $bg_dark;
+        }
+
+        &.light {
+            color: $light;
+        }
+    }
+</style>
