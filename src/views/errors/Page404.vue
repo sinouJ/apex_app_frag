b78db39094f02a767b6d0555396281013921a625
Jordan Sinou <sinoujordan@gmail.com>
2022-03-19 12:18:04 +0100

    404

---
+++ src/views/errors/Page404.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="error">
+        <img src="../../assets/errors/blood.png">
+        <h1>NOT FOUND</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Page404"
+}
+</script>
+
+<style lang="scss">
+    .error {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
