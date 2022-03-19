07452b2dbf00b69f581c64b31ef157c622aa7073
Jordan Sinou <sinoujordan@gmail.com>
2022-03-19 10:14:54 +0100

    Header return

---
+++ src/components/HeaderReturn.vue
@@ -0,0 +1,52 @@
+<template>
+    <header class="header">
+        <Title :title="title"/>
+        <router-link to="profile">
+            <profile-pic :img="img"></profile-pic>
+        </router-link>
+    </header>
+</template>
+
+<script>
+import ProfilePic from '../atoms/ProfilePic.vue'
+import Title from '../atoms/Title.vue'
+
+export default {
+    name: "HeaderReturn",
+    props: {
+        title: String,
+        img: {
+            type: String,
+            default: require('@/assets/icons/white_profile.svg')
+        }
+    },
+    components: {
+        ProfilePic,
+        Title
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    @import '../sass/helpers/variables';
+
+    .header {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        background: $light;
+        position: fixed;
+        top: 0;
+        left: 0;
+        z-index: 999;
+        width: 100%;
+        box-sizing: border-box;
+        padding: 0 2.5%;
+
+        .profilePic {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+    }
+</style>
