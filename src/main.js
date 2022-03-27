488bf129aab91491b7153f45abd968167048dfd5
Jordan Sinou <sinoujordan@gmail.com>
2022-03-28 01:49:38 +0200

    import store gloablly

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
 // Toasts
 import VueNotificationList from '@dafcoe/vue-notification'
 import '@dafcoe/vue-notification/dist/vue-notification.css'
 
-createApp(App).use(router).use(VueNotificationList).mount('#app')
+createApp(App).use(router).use(store).use(VueNotificationList).mount('#app')
