671effe92af61970e3f9d8c5792098950f4d0869
Jordan Sinou <sinoujordan@gmail.com>
2022-09-21 20:29:13 +0200

    init store

---
+++ src/store/modules/games.js
@@ -0,0 +1,25 @@
+// import games from '@/api/games'
+
+const state = {
+    games: Array,
+}
+
+const getters = {
+    games: state => state.games
+}
+
+const actions = {
+
+}
+
+const mutations = {
+
+}
+
+export default {
+    namespaced: true,
+    state,
+    getters,
+    actions,
+    mutations
+}
