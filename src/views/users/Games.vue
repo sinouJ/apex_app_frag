9dd63f6d808fc88d5693a370b9190f6c4ff03140
Jordan Sinou <sinoujordan@gmail.com>
2022-09-25 01:47:48 +0200

    add card game

--- src/views/users/Games.vue
+++ src/views/users/Games.vue
@@ -1,16 +1,62 @@
 <template>
     <div class="games">
-
+        <Header title="games"/>
+        <div class="container">
+            <card-game title="Kill run" class="game" :light="light">
+                <template v-slot:main>
+                    <div class="content">
+                        <p>Le joueur qui fait le plus de frag lors d'une game remporte la manche</p>
+                        <span>
+                            <img src="../../assets/icons/on_play.svg">
+                        </span>
+                    </div>
+                </template>
+            </card-game>
+        </div>
     </div>
 </template>
 
 <script>
+import CardGame from '@/components/Cards/CardGame.vue'
+import Header from '@/components/Header.vue'
+
 export default {
     name: "Games",
+    components: {
+        CardGame,
+        Header,
+    },
+    data() {
+        return {
+            light: false
+        }
+    }
 
 }
 </script>
 
-<style>
+<style lang="scss">
+    @import '../../sass/helpers/variables';
+    
+    .games {
+        .container {
+            .game {
+                background-color: $lightgray;
 
+                .content {
+                    display: flex;
+                    align-items: center;
+
+                    span {
+                        width: 30px;
+                        height: 30px;
+
+                        img {
+                            height: 100%;
+                        }
+                    }
+                }
+            }
+        }
+    }
 </style>
