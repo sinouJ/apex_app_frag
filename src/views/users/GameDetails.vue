f3240845480e1609a1750f8ecb9bf1ff0d13f77f
Jordan Sinou <sinoujordan@gmail.com>
2022-11-15 01:41:56 +0100

    content game detail

--- src/views/users/GameDetails.vue
+++ src/views/users/GameDetails.vue
@@ -1,17 +1,52 @@
 <template>
     <div class="game_details">
-
+        <HeaderReturn title="Game"/>
+        <Loader v-if="loading"/>
+        <div v-else>
+            <div v-if="game.status === 404" class="container">
+                <p>{{game.message}}</p>
+            </div>
+            <div v-else-if="game.status === 500" class="container">
+                <p>{{game.message}}</p>
+            </div>
+            <div v-else class="container">
+                <p>ROUND {{game.game.rounds.length + 1}}</p>
+                <div class="score">
+                    <p>{{game.game.players[0].game_username}}</p>
+                    <p>{{game.game.players[0].player_score}}</p>
+                    <p> - </p>
+                    <p>{{game.game.players[1].game_username}}</p>
+                    <p>{{game.game.players[1].player_score}}</p>
+                </div>
+                <div class="update_score">
+                    round 1 score
+                    <button>Valid score</button>
+                </div>
+                <div class="finish_game">
+                    <button>Finish game</button>
+                    <button>Revenge</button>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import HeaderReturn from '../../components/HeaderReturn'
+import Loader from '../../components/Loader'
+
 import { mapGetters, useStore } from 'vuex'
 
 export default {
     name: "GameDetails",
+    components: {
+        HeaderReturn,
+        Loader
+    },
     data() {
         return {
             id: this.$route.params.id,
+            loading: true,
         }
     },
     computed: {
@@ -19,9 +54,10 @@ export default {
             game: 'game'
         })
     },
-    mounted() {
+    async mounted() {
         const store = useStore()
-        store.dispatch('games/GET_GAME', this.id)
+        await store.dispatch('games/GET_GAME', this.id)
+        this.loading = false
     }
 }
 </script>
